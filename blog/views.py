664fd23ffd121469e3c55a62dc76bd553067851c
dimsonnGH <dimsonn@mail.ru>
2020-07-09 23:55:42 +0400

    Optimize comments count evaluation

--- blog/views.py
+++ blog/views.py
@@ -20,6 +20,20 @@ def serialize_post(post):
     }
 
 
+def serialize_post_optimized(post):
+    return {
+        "title": post.title,
+        "teaser_text": post.text[:200],
+        "author": post.author.username,
+        "comments_amount": post.comments_count,
+        "image_url": post.image.url if post.image else None,
+        "published_at": post.published_at,
+        "slug": post.slug,
+        "tags": [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
 def serialize_tag(tag):
     return {
         'title': tag.title,
@@ -30,21 +44,31 @@ def serialize_tag(tag):
 def index(request):
 
     sorted_posts = Post.objects.prefetch_related('author').annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
-
     most_popular_posts = list(sorted_posts)
+    insert_comments_count_into_QuerySet(most_popular_posts)
 
-    fresh_posts = Post.objects.prefetch_related('author').order_by('published_at')
+    fresh_posts = Post.objects.prefetch_related('author').annotate(comments_amount = Count('comments')).order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
+    insert_comments_count_into_QuerySet(most_fresh_posts)
 
     popular_tags = Tag.objects.annotate(posts_count=Count('posts')).order_by('-posts_count')[:5]
     most_popular_tags = list(popular_tags)
     context = {
-        'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
 
+def insert_comments_count_into_QuerySet(queryset):
+    posts_ids = [post.id for post in queryset]
+
+    posts_with_comments = Post.objects.filter(id__in=posts_ids).annotate(comments_count = Count('comments'))
+    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+    counts_for_id = dict(ids_and_comments)
+
+    for post in queryset:
+        post.comments_count = counts_for_id[post.id]
 
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
